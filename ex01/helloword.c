c8229d43a233ccc69d587d8a2270e9892eda5845
lisizhe-259 <ln425625@qq.com>
2026-06-03 17:54:28 +0800

    ex01:添加键盘输入，显示输出键盘输入内容

--- ex01/helloword.c
+++ ex01/helloword.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdio.h>
 
 int main() {
+    char name[50];
+    
     printf("Hello, World!\n");
+        
+    printf("ÇëÊäÈëÄãµÄÃû×Ö: ");
+     
+    fgets(name, sizeof(name), stdin);
+    
+    printf("Hello, ");
+    printf("%s", name);
+    
     return 0;
 }
 
