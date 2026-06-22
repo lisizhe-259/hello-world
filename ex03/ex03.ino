71dc27e773c3f99f30f01cfc9ffa419024320d0d
lisizhe-259 <ln425625@qq.com>
2026-06-22 19:58:47 +0800

    用 millis() 函数控制LED产生SOS闪烁信息

--- ex03/ex03.ino
+++ ex03/ex03.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+// 定义LED引脚
+const int ledPin = 2;  
+const int freq = 5000;
+const int resolution = 8;
 
+// 闪烁控制变量
+unsigned long previousMillis = 0;
+int step = 0;
+int sequence[] = {200, 200, 200, 200, 200, 600,   // S: 点-灭-点-灭-点-长灭
+                  600, 200, 600, 200, 600, 600,    // O: 划-灭-划-灭-划-长灭
+                  200, 200, 200, 200, 200, 2000};  // S: 点-灭-点-灭-点-长停顿
+bool ledState = false;
+
+void setup() {
+  Serial.begin(115200);
+  ledcAttach(ledPin, freq, resolution);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis();
 
+  if (currentMillis - previousMillis >= sequence[step]) {
+    previousMillis = currentMillis;
+
+    ledState = !ledState;
+    ledcWrite(ledPin, ledState ? 255 : 0);
+
+    step++;
+    if (step >= 18) {
+      step = 0;
+      Serial.println("SOS cycle completed");
+    }
+  }
 }
